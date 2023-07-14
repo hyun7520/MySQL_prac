a8846dad9da0eaccb897a599a5d8c14938b673be
hyun7520 <sanghyun123452@gmail.com>
2023-07-14 18:26:38 +0900

    update SQL study

---
+++ MySQL/Prac3 Refining Selections/LIKE.sql
@@ -0,0 +1,56 @@
+SELECT 
+    title, author_fname, author_lname
+FROM
+    books
+WHERE
+    author_fname LIKE '%DA%';
+
+SELECT 
+    *
+FROM
+    books
+WHERE
+    title LIKE '%:%';
+    
+SELECT 
+    *
+FROM
+    books
+WHERE
+    author_fname LIKE '____';
+    
+SELECT 
+    *
+FROM
+    books
+WHERE
+    author_fname LIKE '_a_';
+
+SELECT 
+    title
+FROM
+    books
+WHERE
+    title LIKE '%\%%';
+
+SELECT title, author_fname, author_lname, pages 
+FROM books
+WHERE author_fname LIKE '%da%';
+ 
+SELECT title, author_fname, author_lname, pages 
+FROM books
+WHERE title LIKE '%:%';
+ 
+SELECT * FROM books
+WHERE author_fname LIKE '____';
+ 
+SELECT * FROM books
+WHERE author_fname LIKE '_a_';
+
+-- To select books with '%' in their title:
+SELECT * FROM books
+WHERE title LIKE '%\%%';
+ 
+-- To select books with an underscore '_' in title:
+SELECT * FROM books
+WHERE title LIKE '%\_%';
--- MySQL/Prac3 Refining Selections/ORDER BY.sql
+++ MySQL/Prac3 Refining Selections/ORDER BY.sql
@@ -18,3 +18,21 @@ ORDER BY author_lname DESC;
  
 SELECT * FROM books
 ORDER BY released_year;
+
+SELECT 
+    book_id, author_fname, author_lname, pages
+FROM
+    books
+ORDER BY 2;
+
+SELECT 
+    book_id, author_fname, author_lname, pages
+FROM
+    books
+ORDER BY author_fname DESC , pages;
+
+SELECT 
+    CONCAT(author_fname, ' ', author_lname) AS author
+FROM
+    books
+ORDER BY author;
