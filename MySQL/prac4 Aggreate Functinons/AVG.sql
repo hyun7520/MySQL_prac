f35e941a7855319cd9c3441d2a33f5dadc03b11f
hyun7520 <sanghyun123452@gmail.com>
2023-07-21 20:14:30 +0900

    update SQL study

---
+++ MySQL/prac4 Aggreate Functinons/AVG.sql
@@ -0,0 +1,30 @@
+SELECT 
+    AVG(released_year)
+FROM
+    books;
+    
+SELECT 
+    AVG(pages)
+FROM
+    books;
+
+SELECT 
+    AVG(stock_quantity)
+FROM
+    books;
+    
+SELECT 
+    AVG(stock_quantity)
+FROM
+    books
+GROUP BY released_year;
+
+SELECT AVG(pages) FROM books;
+ 
+SELECT AVG(released_year) FROM books;
+ 
+SELECT 
+    released_year, 
+    AVG(stock_quantity), 
+    COUNT(*) FROM books
+GROUP BY released_year;
