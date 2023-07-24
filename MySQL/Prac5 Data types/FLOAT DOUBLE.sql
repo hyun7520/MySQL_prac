dd9576532f681b32e39446eae6d121c9c8cf694d
hyun7520 <sanghyun123452@gmail.com>
2023-07-24 21:08:55 +0900

    update SQL study
    setup

---
+++ MySQL/Prac5 Data types/FLOAT DOUBLE.sql
@@ -0,0 +1,9 @@
+CREATE TABLE numbers (x FLOAT, y DOUBLE);
+
+INSERT INTO numbers (x,y) VALUES (1.123, 1.123); 
+
+SELECT * FROM numbers;
+
+INSERT INTO numbers (x, y) VALUES (1.12345678, 1.12345678);
+
+SELECT * FROM numbers;
