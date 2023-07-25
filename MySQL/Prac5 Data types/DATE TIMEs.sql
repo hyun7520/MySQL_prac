58118c85e4d0f5f232778ce275bd3c45468282de
hyun7520 <sanghyun123452@gmail.com>
2023-07-25 17:27:44 +0900

    Update DATE TIMEs.sql

--- MySQL/Prac5 Data types/DATE TIMEs.sql
+++ MySQL/Prac5 Data types/DATE TIMEs.sql
@@ -10,7 +10,27 @@ VALUES ('Elton', '2000-12-25', '11:00:00', '2000-12-25 11:00:00'),
 ('Lulu', '1985-04-11', '9:45:10', '1985-04-11 9:45:10'),
 ('Juan', '2020-08-15', '23:59:00', '2020-08-15 23:59:00');
 
+INSERT INTO people (name, birthdate, birthtime, birthdt) VALUES ('Hazel', CURDATE(), CURTIME(), NOW());
+
 SELECT 
     *
+FROM
+    people;
+
+SELECT 
+    birthdate,
+    DAY(birthdate),
+    DAYOFWEEK(birthdate),
+    DAYOFYEAR(birthdate)
+FROM
+    people;
+    
+SELECT 
+    birthdate, MONTHNAME(birthdate)
+FROM
+    people;
+    
+SELECT 
+    name, birthtime, HOUR(birthtime)
 FROM
     people;
